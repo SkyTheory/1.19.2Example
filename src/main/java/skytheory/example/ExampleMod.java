b2c19476ff850c9a9fcfe241df460bc091348ae4
SkyTheory <koolong1001@gmail.com>
2022-10-09 02:56:25 +0900

    Update ExampleMod.java

--- src/main/java/skytheory/example/ExampleMod.java
+++ src/main/java/skytheory/example/ExampleMod.java
@@ -4,19 +4,10 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import skytheory.example.event.EntityEvent;
 import skytheory.example.event.SetupEvent;
 import skytheory.example.init.BlockInit;
@@ -32,16 +23,7 @@ public class ExampleMod
 	public static final String MODID = "st_example";
 	// Directly reference a slf4j logger
 	public static final Logger LOGGER = LogUtils.getLogger();
-	// Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-	// Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-	// Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-	public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-	// Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-	public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-
+	
 	public ExampleMod() {
 		// EventやDeferredRegisterなどを登録するEventBusを取得する
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
